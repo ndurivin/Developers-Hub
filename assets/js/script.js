0d347ddc58db36813472557b3c1a26991496088a
Vinn Nduri <ndurivo@gmail.com>
2022-06-23 10:32:22 +0300

    Add JS code to display developer names

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+// Developers Section 
+
+document.getElementById('users').addEventListener('mouseover', getNames);
+
+
+
+
+function getNames(){
+    const url = 'https://api.github.com/users'
+    fetch(url)
+    .then(res => res.json())
+    .then(devs => {
+        devs.forEach((dev) => {
+            let ul = document.getElementById('users')
+            let li = document.createElement('li')
+            li.className = 'users-list'
+            li.innerText = dev.login;
+            ul.appendChild(li)
+            
+
+        })
+    })
+    .catch((err) => err)
+
+}
+getNames()
+    
