1e44852a0a498119fa0b16799e14f3afc2233cd8
Vinn Nduri <ndurivo@gmail.com>
2022-06-25 21:22:44 +0300

    Additional fixws on page view and functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,19 +33,6 @@ function getNames(){
     .catch((err) => err)
 }
 
-// Developer profiles section 
-let name = document.getElementById('devName')
-let image = document.getElementById('image')
-let tag = document.getElementById('devTag')
-let followers = document.getElementById('followers')
-let following = document.getElementById('following')
-let repos = document.getElementById('repos')
-let expertise1 = document.getElementById('exp1')
-let expertise2 = document.getElementById('exp2')
-let expertise3 = document.getElementById('exp3')
-let expertise4 = document.getElementById('exp4')
-let expertise5 = document.getElementById('exp5')
-
 
 function getProfiles(){
     fetch(url)
