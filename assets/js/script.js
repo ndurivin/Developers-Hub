159222847e0d09bc0189eb2dec3859e6324fd856
Vinn Nduri <ndurivo@gmail.com>
2022-06-27 13:14:19 +0300

    Draft work

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const usersDiv = document.getElementById('btnToggle').addEventListener('click', 
 function displayDiv(){
     if (userNames.style.display === 'none') {
         userNames.style.display = 'block';
-        alert('Click on specific user to view profile')
+        alert('Click on specific name to view user profile')
     } else {
         userNames.style.display = 'none';
     }
